c416f3e7cb224b3902ee80fa46ab9ff851a1f29c
David Ajanaku <davidajanaku@Davids-MacBook-Pro.local>
2025-07-25 19:07:16 +0100

    Removed _redirects and netlify.toml files, updated vite.config.js with build and preview settings.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  preview: {
+    port: 4173,
+    strictPort: true,
+  },
   server: {
     port: 5173,
-  },
-});
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:5173"
+  }
+})
