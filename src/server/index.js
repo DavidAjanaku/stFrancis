b70e30cd22f396eae8cd98b3dd4f64662b2fcfc2
David Ajanaku <davidajanaku46163@gmail.com>
2025-08-25 19:08:06 +0100

    Removed deprecated options from MongoDB connection settings in index.js

--- src/server/index.js
+++ src/server/index.js
@@ -129,8 +129,7 @@ const connectDB = async () => {
       maxPoolSize: 10, // Maintain up to 10 socket connections
       serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
       socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-      bufferCommands: false, // Disable mongoose buffering
-      bufferMaxEntries: 0 // Disable mongoose buffering
+      // Removed deprecated options: bufferCommands and bufferMaxEntries
     });
     console.log('MongoDB connected successfully');
   } catch (error) {
