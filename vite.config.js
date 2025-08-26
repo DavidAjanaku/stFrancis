dc63a25c950a17eb349debb65ec7160521909f70
David Ajanaku <davidajanaku46163@gmail.com>
2025-08-26 13:38:17 +0100

    Update vite.config.js to include allowedHosts in preview configuration

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,11 @@ export default defineConfig({
   preview: {
     port: 4173,
     strictPort: true,
+    allowedHosts: [
+      'stfrancis-52b1.onrender.com',
+      'stfrancis-1.onrender.com',
+      'distinct-stranger-production.up.railway.app'
+    ]
   },
   server: {
     port: 5173,
